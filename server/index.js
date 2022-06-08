28858a11b253baa8354b3651f098bf12f364f7d7
markgmz92 <markgmz92@gmail.com>
2022-06-08 23:52:47 +0800

    [PMA - 002] Refactoring code

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { graphqlHTTP } from 'express-graphql';
+const express = require('express');
+const dotenv = require('dotenv');
+const { graphqlHTTP } = require('express-graphql');
+const { schema } = require('./schema/schema');
 const port = process.env.PORT || 5000;
 
 dotenv.config();
 const app = express();
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development'
+  })
+);
 
 app.listen(port, console.log(`Server is running on port ${port}`));
